4b174bc2fc9a599982b81824a80bc0498752ecf5
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-10 17:10:58 -0400

    ainda na construção da page inicial do projeto, pausando estudos para ir para auto escola

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,93 @@
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
+import { useState } from 'react';
+
 export function App() {
+  const [isGuestInputOpen, setIsGuestInputOpen] = useState(false);
+
+  function openGuestInput() {
+    setIsGuestInputOpen(true);
+  }
+
     return (
-      <div className="text-3xl font-bold">
-        <h1>Salve React</h1>
+      <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+        <div className="max-w-3xl w-full px-6 text-center space-y-10">
+          <div className="flex flex-col items-center gap-3">
+            <img src="/logo.svg" alt="planner" />
+            <p className="text-zinc-300 text-lg">
+              Convide seus amigos e planeje sua próxima viagem
+            </p>
+          </div>
+
+          <div className="space-y-4">
+            <div className="h-16 bg-zinc-900 px-4 rounded-lg flex items-center gap-3 shadow-shape">
+              <div className="flex items-center gap-2 flex-1">
+                <MapPin className="size-5 text-zinc-400" />
+                <input
+                  disabled={isGuestInputOpen}
+                  type="text"
+                  placeholder="Para onde voce vai?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <Calendar className="size-5 text-zinc-400" />
+                <input
+                  disabled={isGuestInputOpen}
+                  type="text"
+                  placeholder="Quando?"
+                  className="bg-transparent text-lg placeholder-zinc-400 w-20 outline-none"
+                />
+              </div>
+              <div className="w-px h-6 bg-zinc-800"></div>
+              {isGuestInputOpen ? (
+              <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+                Alterar Local/data
+                <Settings2 className="size-5" />
+              </button>
+              ) : (
+              <button
+                onClick={openGuestInput}
+                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+              >
+                Continuar
+                <ArrowRight className="size-5" />
+              </button>
+              )}
+            </div>
+
+            {isGuestInputOpen && (
+              <div className="h-16 bg-zinc-900 px-4 rounded-lg flex items-center gap-3 shadow-shape">
+                <div className="flex items-center gap-2 flex-1">
+                  <UserRoundPlus className="size-5 text-zinc-400" />
+                  <input
+                    type="text"
+                    placeholder="Quem estará na viagem?"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <div className="w-px h-6 bg-zinc-800"></div>
+                <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                  Confirmar viagem
+                  <ArrowRight className="size-5" />
+                </button>
+              </div>
+            )}
+          </div>
+
+          <p className="text-zinc-500">
+            Ao planejar sua viagem pela planner voce automaticamente concorda{" "}
+            <br /> com nossos{" "}
+            <a className="text-zinc-300" href="#">
+              termos de uso
+            </a>{" "}
+            e{" "}
+            <a className="text-zinc-300" href="#">
+              política de privacidade
+            </a>
+            .
+          </p>
+        </div>
       </div>
-    )
+    );
 }
